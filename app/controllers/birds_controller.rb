afae9feee56e8dc2cad97ad3871785001ee6da57
Charles Swaleh <charlswall@live.com>
2023-04-24 15:44:09 +0300

    completed resources create

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -16,4 +16,15 @@ class BirdsController < ApplicationController
     end
   end
 
+  # POST /birds
+  def create
+    bird = Bird.create(production_parameters)
+    render json: bird, status: :created
+  end
+
+  private
+
+  def production_parameters
+    params.permit(:name, :species)
+  end
 end
